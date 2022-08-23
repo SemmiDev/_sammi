8344bd6098ab85bebeb0d19edf0cf53d1b924f8a
Sammi <sammidev4@gmail.com>
2022-08-23 07:32:34 +0700

    init

--- script.js
+++ script.js
@@ -9,39 +9,11 @@ window.onscroll = function () {
     }
 }
 
+const buttonku = document.getElementById('buttonku');
+buttonku.addEventListener('click', function () {
+    const gambarku = document.getElementById('gambarku');
+    const tentangku = document.getElementById('about');
 
-//Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('aktif-hamburger');
-    navMenu.classList.toggle('hidden');
-
-});
-
-const buttonCloseNotif = document.querySelector("#close-notif-berhasil");
-const tulisanBerhasil = document.querySelector("#notif-berhasil");
-const loading = document.querySelector("#loading");
-
-
-buttonCloseNotif.addEventListener('click', function () {
-    tulisanBerhasil.classList.toggle("hidden");
-});
-
-const scriptURL = "https://script.google.com/macros/s/AKfycbwqljeJBO2twb5X9P14CytVsTtGhG-RsCBofA27O0kQjD9Popj-xFFGBZoHFAs_VPxJ/exec";
-const form = document.forms["contact-form"];
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    loading.classList.toggle("hidden");
-    fetch(scriptURL, {
-        method: "POST",
-        body: new FormData(form)
-    })
-        .then((response) => {
-            console.log("Success!", response);
-            loading.classList.toggle("hidden");
-            tulisanBerhasil.classList.toggle("hidden");
-            form.reset();
-        })
-        .catch((error) => console.error("Error!", error.message));
+    gambarku.classList.toggle('hidden');
+    tentangku.classList.toggle('hidden');
 });
