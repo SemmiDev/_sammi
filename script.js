31c1d3a8a460deea5eda0def92a7b418ddb0a2da
Sammi <sammidev4@gmail.com>
2022-08-23 11:30:46 +0700

    init

--- script.js
+++ script.js
@@ -10,12 +10,28 @@ window.onscroll = function () {
 }
 
 const buttonku = document.getElementById('buttonku');
+const buttonku2 = document.getElementById('buttonku2');
+
 buttonku.addEventListener('click', function () {
     const gambarku = document.getElementById('gambarku');
     const about = document.getElementById('about');
     const footer = document.getElementById('footer');
+    const home = document.getElementById('home');
 
     gambarku.classList.toggle('hidden');
     about.classList.toggle('hidden');
     footer.classList.toggle('hidden');
-}); 
+    home.classList.toggle('hidden');
+});
+
+buttonku2.addEventListener('click', function () {
+    const gambarku = document.getElementById('gambarku');
+    const about = document.getElementById('about');
+    const footer = document.getElementById('footer');
+    const home = document.getElementById('home');
+
+    gambarku.classList.toggle('hidden');
+    about.classList.toggle('hidden');
+    footer.classList.toggle('hidden');
+    home.classList.toggle('hidden');
+});
